55bc0b351e184ed7962600bd2170c2ac7837cbf7
Abrahão Eneias <abrahao.eneias@MacBook-Air.local>
2020-05-12 18:49:05 -0300

    Adicao do botao Calcular e Info

--- lib/main.dart
+++ lib/main.dart
@@ -15,35 +15,55 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora de IMC"),
-        centerTitle: true,
-        backgroundColor: Colors.purple,
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.refresh), onPressed: () {}),
-        ],
-      ),
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Icon(Icons.person_outline, size: 120.0, color: Colors.purple),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Seu Peso (kg)",
-                  labelStyle: TextStyle(color: Colors.purple)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.purple, fontSize: 25.0),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Sua Altura (cm)",
-                  labelStyle: TextStyle(color: Colors.purple)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.purple, fontSize: 25.0),
-            ),
-          ]),
-    );
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.refresh), onPressed: () {}),
+          ],
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(Icons.person_outline, size: 120.0, color: Colors.purple),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Seu Peso (kg)",
+                      labelStyle: TextStyle(color: Colors.purple)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.purple, fontSize: 25.0),
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Sua Altura (cm)",
+                      labelStyle: TextStyle(color: Colors.purple)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.purple, fontSize: 25.0),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: Container(
+                      height: 50.0,
+                      child: RaisedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Calcular",
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        ),
+                        color: Colors.purple,
+                      )),
+                ),
+                Text(
+                  "Info",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.purple, fontSize: 25.0),
+                )
+              ]),
+        ));
   }
 }
